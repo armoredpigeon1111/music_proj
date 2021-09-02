7640660396a52e4e054c2c0178b7d862494f211a
armoredpigeon1111 <armoredpigeon1111@outlook.com>
2021-09-02 17:06:23 -0400

    Added SongDetail view with get_object and get methods

--- music/views.py
+++ music/views.py
@@ -1,3 +1,4 @@
+from django.http.response import Http404
 from django.shortcuts import render
 from .models import Song
 from .serializers import SongSerializer
@@ -19,3 +20,18 @@ class SongList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class SongDetail(APIView):
+    
+    def get_object(self, pk):
+        try:
+            return Song.objects.get(pk=pk)
+        except Song.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        song = self.get_object(pk)
+        serializer = SongSerializer(song)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+   
